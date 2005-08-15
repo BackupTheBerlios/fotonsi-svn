65ab03418836a14d5a48ade946fca0190e122b05
zoso <zoso@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2005-08-15 18:31:17 +0000

    - Creado el método de clase «validArgs» para ir añadiendo en cada control los   argumentos válidos que se pueden pasar al control

--- remix_widgets/lib/remix_widgets/widgets.rb
+++ remix_widgets/lib/remix_widgets/widgets.rb
@@ -4,12 +4,26 @@ module RemixWidgets
       class Base
          attr_reader :form, :attrs
 
+         # Class method to store valid argument lists
+         def self.validArgs(*list)
+            if list.size > 0
+               dad = ancestors[1]
+               @validArgs = dad.respond_to?(:validArgs) ? dad.validArgs : []
+               @validArgs += list
+            end
+            @validArgs
+         end
+         validArgs "style", "class", "id"
+
+         # Creates a new widget. Receives the form it's in, and the attribute
+         # hash
          def initialize(form, attrs)
             @form  = form
             @attrs = attrs
          end
 
          # Basic attrs
+
          def name; @attrs[:name]; end
          def widgetType
             self.class.name.sub(/^RemixWidgets::Widgets::/, '')
@@ -17,9 +31,14 @@ module RemixWidgets
          def page; @form.page; end
 
          # Widget hooks
+
          def form_type_init; end
          def page_type_init; end
          def widget_init; end
+
+         # Regular methods
+
+         def render; "[#{name}: please overload the 'render' method for widget type '#{widgetType}']"; end
       end
    end
 end
