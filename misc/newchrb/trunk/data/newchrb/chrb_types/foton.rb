490778b65b6481da07844af44ccfa224f3108c69
zoso <zoso@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2006-05-18 20:56:59 +0000

    - Changed the list of generated files to /etc/hostname and /etc/mailname

--- misc/newchrb/trunk/data/newchrb/chrb_types/foton.rb
+++ misc/newchrb/trunk/data/newchrb/chrb_types/foton.rb
@@ -3,7 +3,7 @@ require 'chrb_types/base'
 module ChrbTypes
     class Foton < ChrbTypes::Base
         property      :intranet, :description => "Is the new chroot going to be installed in Foton's intranet?", :default => "y", :type => :boolean
-        erb_templates '/etc/hosts',     # Procesa /etc/hosts.erb -> /etc/hosts
-                      '/etc/apt/sources.list'
+        erb_templates '/etc/hostname',
+                      '/etc/mailname'
     end
 end
