70e69cbcd6d82b68983ea5c90b5bd095c7a91468
zoso <zoso@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2006-05-18 12:36:58 +0000

    - Make «class_to_filename» and «filename_to_class» understand paths and double   colons

--- ruby/foton/trunk/lib/foton/misc.rb
+++ ruby/foton/trunk/lib/foton/misc.rb
@@ -3,11 +3,13 @@
 # This function converts a class name to a filename containing that class,
 # according to the standard Ruby conventions
 def class_to_filename (c)
-   c.gsub(/([A-Z])/, '_\1').sub(/_/, '').downcase
+   c.split(/::/).map {|i| i.gsub(/([A-Z])/, '_\1').sub(/_/, '').downcase}.
+                 join('/')
 end
 
 # This function converts a filename containing a class to the name of that
 # class, according to the standard Ruby conventions
 def filename_to_class (f)
-   f.capitalize.gsub(/_([a-z])/) {|s| $1.upcase }
+   f.split('/').map {|i| i.capitalize.gsub(/_([a-z])/) {|s| $1.upcase }}.
+                join('::')
 end
--- ruby/foton/trunk/test/tc_foton.rb
+++ ruby/foton/trunk/test/tc_foton.rb
@@ -16,4 +16,16 @@ class TC_Foton <Test::Unit::TestCase
                                                 "filename_to_class and back")
       end
    end
+
+   def test_class_to_filename_with_paths
+      fileList = [['some/easy_file', 'Some::EasyFile'],
+                  ['ibm/foo/bar', 'Ibm::Foo::Bar'],
+                  ['i_b/m', 'IB::M']]
+      fileList.each do |f|
+         assert_equal(f[1], filename_to_class(f[0]),
+                                                "filename_to_class")
+         assert_equal(f[0], class_to_filename(filename_to_class(f[0])),
+                                                "filename_to_class and back")
+      end
+   end
 end
