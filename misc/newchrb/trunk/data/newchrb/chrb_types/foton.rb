7e19375262d5b6000b3f53368dbf4ba23a28e207
zoso <zoso@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2006-05-22 23:05:46 +0000

    - Also ask IP address for Fotón's new chrbs

--- misc/newchrb/trunk/data/newchrb/chrb_types/foton.rb
+++ misc/newchrb/trunk/data/newchrb/chrb_types/foton.rb
@@ -3,6 +3,7 @@ require 'chrb_types/base'
 module ChrbTypes
     class Foton < ChrbTypes::Base
         property      :intranet, :description => "Is the new chroot going to be installed in Foton's intranet?", :default => "y", :type => :boolean
+        property      :ip,       :description => "IP address (only for Foton's intranet)?"
         erb_templates '/etc/hostname',
                       '/etc/mailname'
     end
