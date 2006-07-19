d8ac3b7cc15d9b076a2ae62872e74f897bafef85
alberto <alberto@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2006-07-19 19:59:09 +0000

    Versión inicial de la aplicación de prueba de la DLL

---
+++ win32/apli_huella/apli_huella.cpp
@@ -0,0 +1,71 @@
+// apli_huella.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "apli_huella.h"
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+#include "NBioAPI.h"
+#include "NBioAPI_CheckValidity.h"
+
+typedef NBioAPI_RETURN (*importFunction_LPCTSTR)(LPCTSTR a);
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	// initialize MFC and print and error on failure
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+		nRetCode = 1;
+	}
+	else
+	{
+
+		// Load DLL file
+		HINSTANCE hinstLib = LoadLibrary("huella.dll");
+		if (hinstLib == NULL) {
+				printf("ERROR: unable to load DLL\n");
+				return 1;
+		}
+
+
+		// Get function pointer
+		importFunction_LPCTSTR FotoNBioAPI_CheckValidity;
+		FotoNBioAPI_CheckValidity = (importFunction_LPCTSTR) GetProcAddress(hinstLib, "FotoNBioAPI_CheckValidity");
+		if (FotoNBioAPI_CheckValidity == NULL) {
+				printf("ERROR: unable to find DLL function\n");
+				return 1;
+		}
+
+		cout << "Llamado a la funcion FotoNBioAPI_CheckValidity\n";
+		NBioAPI_RETURN ret = FotoNBioAPI_CheckValidity((LPCTSTR)"NBioBSP.dll");
+
+		if (ret == NBioAPIERROR_NONE)
+			cout << "DLL OK\n";
+		else
+			cout << "ERROR\n";
+
+		cout << "Presione ENTER para finalizar...\n" << endl;
+
+		getchar();
+
+		// Unload DLL file
+		FreeLibrary(hinstLib);
+		
+		nRetCode = 0;
+	}
+
+	return nRetCode;
+}
