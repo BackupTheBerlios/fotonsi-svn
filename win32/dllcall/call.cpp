69601c64139c2b26a3f9a6b14d88f56808720bcd
alberto <alberto@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2006-07-27 10:31:36 +0000

    Añado la funcion para que Juan pruebe a recibir un struct de 6 UINT32.

--- win32/dllcall/call.cpp
+++ win32/dllcall/call.cpp
@@ -4,15 +4,26 @@
 
 //extern "C" __declspec(dllimport) int suma(int a, int b);
 
+/* test_struct */
+typedef struct test_struct {
+	UINT32      f;  /* must be 0 */
+	UINT32      e;     /* Read Only */
+	UINT32      d;    /* Read Only */
+	UINT32      c;
+	UINT32      b;
+	UINT32      a;
+} TEST_STRUCT;
 
 //explicit runtime linking
 // DLL function signature
 typedef int (*importFunction)(int, int);
+typedef TEST_STRUCT (*fp_test)(UINT32 a, UINT32 b, UINT32 c, UINT32 d, UINT32 e, UINT32 f);
 
 int
 main()
 {
     importFunction addNumbers;
+	fp_test test;
     int result;
 
     // Load DLL file
@@ -29,6 +40,13 @@ main()
             return 1;
     }
 
+    // Get function pointer
+    test = (fp_test)GetProcAddress(hinstLib, "test");
+    if (test == NULL) {
+            printf("ERROR: unable to find DLL function\n");
+            return 1;
+    }
+
 	int a, b;
 	char c;
 
@@ -40,6 +58,9 @@ main()
 	cin >> b;
 
 	cout << "Suma A+B = " << addNumbers(a,b) << "\n\n";
+
+	TEST_STRUCT st = test(1,2,3,4,5,6);
+
 	cout << "Presione ENTER para finalizar...\n" << endl;
 
 	getchar();
--- win32/testdll/testdll.cpp
+++ win32/testdll/testdll.cpp
@@ -3,7 +3,18 @@
 
 #include "stdafx.h"
 
+/* test_struct */
+typedef struct test_struct {
+	UINT32      f;  
+	UINT32      e;   
+	UINT32      d;    
+	UINT32      c;
+	UINT32      b;
+	UINT32      a;
+} TEST_STRUCT;
+
 extern "C" __declspec(dllexport)int suma(int A, int B);
+extern "C" __declspec(dllexport)TEST_STRUCT test(UINT32 a, UINT32 b, UINT32 c, UINT32 d, UINT32 e, UINT32 f);
 
 BOOL APIENTRY DllMain( HANDLE hModule, 
                        DWORD  ul_reason_for_call, 
@@ -16,4 +27,16 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 int suma(int A, int B)
 {
     return A+B;
+}
+
+TEST_STRUCT test(UINT32 a, UINT32 b, UINT32 c, UINT32 d, UINT32 e, UINT32 f)
+{
+	TEST_STRUCT st;
+	st.a=a;
+	st.b=b;
+	st.c=c;
+	st.d=d;
+	st.e=e;
+	st.f=f;
+    return st;
 }
