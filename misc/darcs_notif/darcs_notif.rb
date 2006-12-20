1ac8dea555bca925b50706db9d18bbc026043860
setepo <setepo@9e7f320e-6cfd-0310-95aa-b8e6a9f52728>
2006-12-20 13:30:39 +0000

    - Script para mandar notificaciones de cambios en el darcs por email

---
+++ misc/darcs_notif/darcs_notif.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+
+require 'net/smtp'
+
+Dir["*"].each {|basedir|
+    next unless File.directory?(basedir + "/_darcs")
+
+    patchs = []
+    darcs = `darcs changes --repodir="#{basedir}" -v --matches 'date "10 minutes ago"'`
+    darcs.each_line {|line|
+        patchs << '' if /^\w+ \w+.*@.*$/ === line    # Usamos esta regex para separar los parches
+        patchs.last << line
+    }
+
+    patchs.each {|patch|
+        # Enviamos los emails para este 
+        Net::SMTP.start('localhost', 25) { |smtp|
+            # Analizamos el parche.
+            # La primera línea será la fecha y el autor del commit
+            # La segunda el nombre del parche
+            # El resto... pues el resto
+
+            if /^(.*?)\s+(\S+@\S+)\s*$\s*\*?\s*(.*)$/ === patch
+                date = $1
+                author = $2
+                name = $3
+                msg = ''
+
+                msg << "From: #{author}\n"
+                msg << "To: tecnicos@foton.es\n"
+                msg << "Subject: [darcs fotón] [#{basedir}] #{name}\n"
+                msg << "\n\n"
+                msg << patch
+
+                smtp.send_message msg, author, 'tecnicos@foton.es'
+            end
+
+        }
+    }
+}
